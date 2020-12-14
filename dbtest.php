3cacc7f16a597ffccb109e5d3f2b2a92c3101114
Sanjil Shrestha <75104586+sanjiltbd@users.noreply.github.com>
2020-12-14 23:50:02 +0700

    Change to mysqli_connect

--- dbtest.php
+++ dbtest.php
@@ -6,7 +6,7 @@ $dbpwd = getenv("dbrootpass");
 $dbname = getenv("dbname");
 
 
-$connection = mysql_connect($dbhost.":".$dbport, $dbuser, $dbpwd);
+$connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd);
 
 if(!$connection) {
 	echo "Could not connect to database.";
